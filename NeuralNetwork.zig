2be4b08baad257de96d70f8ed31669958628b976
William Rescigno <143554483+will2web@users.noreply.github.com>
2025-03-22 12:05:11 -0700

    Weights by Random Number Generator
    I was just learning how to use zig's random number generator to generate the neural network weights

--- NeuralNetwork.zig
+++ NeuralNetwork.zig
@@ -1,11 +1,33 @@
 const std = @import("std");
+//const RandomNumGenerate = std.rand.DefaultPrng.init(42);
 
 pub fn main() !void {
+    var seed: u64 = undefined;
+    std.posix.getrandom(std.mem.asBytes(&seed)) catch |err| {
+        std.debug.print("Failed to get random seed: {}\n", .{err});
+        return;
+    };
+
+    var prng = std.Random.DefaultPrng.init(seed);
+    var rand = prng.random();
+
+    //below just for testing/debuging random number generator
+    // Generate a random float between 0 and 1
+    //const value = rand.float(f32);
+    //std.debug.print("Random value: {}\n", .{value});
+    //std.debug.print("My random number is: {d}\n", .{value});
+
+    const weight1 = rand.float(f32);
+    const weight2 = rand.float(f32);
+    const weight3 = rand.float(f32);
     const learningRate = 1;
     const bias = 1;
-    var x: i3 = undefined;
 
+    var x: i3 = undefined;
     x = learningRate + bias;
 
-    std.debug.print("{d}\n", .{x});
+    std.debug.print("Learning Rate plus Bias Equals {d}\n", .{x});
+    std.debug.print("Weight 1 is : {d}\n", .{weight1});
+    std.debug.print("Weight 2 is : {d}\n", .{weight2});
+    std.debug.print("Weight 3 is : {d}\n", .{weight3});
 }
