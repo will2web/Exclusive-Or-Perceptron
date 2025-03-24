574bc0857b85d908c4d7701a58c35e44ee2898dd
William Rescigno <143554483+will2web@users.noreply.github.com>
2025-03-23 17:33:31 -0700

    Outlined perceptron function

--- NeuralNetwork0-2.zig
+++ NeuralNetwork0-2.zig
@@ -1,26 +1,22 @@
 const std = @import("std");
-//const RandomNumGenerate = std.rand.DefaultPrng.init(42);
+var seed: u64 = undefined;
+var rand = prng.random();
+var prng = std.Random.DefaultPrng.init(seed);
+const weight1 = rand.float(f32);
+const weight2 = rand.float(f32);
+const weight3 = rand.float(f32);
+const learningRate = 1;
+const bias = 1;
+
+const weights = [_]f32{ weight1, weight2, weight3 };
 
 pub fn main() !void {
-    var seed: u64 = undefined;
     std.posix.getrandom(std.mem.asBytes(&seed)) catch |err| {
         std.debug.print("Failed to get random seed: {}\n", .{err});
         return;
     };
 
     std.debug.print("Seed address is: {}\n", .{&seed});
-
-    var prng = std.Random.DefaultPrng.init(seed);
-    var rand = prng.random();
-
-    const weight1 = rand.float(f32);
-    const weight2 = rand.float(f32);
-    const weight3 = rand.float(f32);
-    const learningRate = 1;
-    const bias = 1;
-    const weights = [_]f32{ weight1, weight2, weight3 };
-
-    std.debug.print("Data type for bias is {}\n", .{@TypeOf(bias)});
 
     var x: i3 = undefined;
     x = learningRate + bias;
@@ -32,15 +28,11 @@ pub fn main() !void {
 
     std.debug.print("len: {}\n", .{weights.len});
     std.debug.print("\nMy Weights are: {any}\n", .{weights});
-
-    perceptron(weights, bias, 3, 4, 5);
 }
 
-fn perceptron(weights: [3]f32, bias: comptime_int, input1: f32, input2: f32, output: f32) void {
-    const outputP: f32 = input1 * weights[0] + input2 * weights[1] + bias * weights[2];
-    std.debug.print("outputP is {}\n", .{outputP});
-    std.debug.print("output is {}\n", .{output});
-}
+// fn perceptron(input1: f32, input2: f32, output: f32) void {
+//     const outputP: f32 = input1 * weights[0];
+// }
 
 // def Perceptron(input1, input2, output) :
 //    outputP = input1*weights[0]+input2*weights[1]+bias*weights[2]
