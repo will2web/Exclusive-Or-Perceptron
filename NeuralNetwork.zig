de643cbdda73f1d2cd1b6e702741eab40a84209e
William Rescigno <143554483+will2web@users.noreply.github.com>
2025-03-29 10:04:11 -0700

    Eliminated redundant variables

--- NeuralNetwork.zig
+++ NeuralNetwork.zig
@@ -1,8 +1,12 @@
 const std = @import("std");
 const learningRate = 1;
+var seed: u64 = undefined;
+
+//Trying to move this array out of mainn
+//var weights = [_]f32{ rand.float(f32), rand.float(f32), rand.float(f32) };
 
 pub fn main() !void {
-    var seed: u64 = undefined;
+    //var seed: u64 = undefined;
     std.posix.getrandom(std.mem.asBytes(&seed)) catch |err| {
         std.debug.print("Failed to get random seed: {}\n", .{err});
         return;
@@ -11,16 +15,8 @@ pub fn main() !void {
     var prng = std.Random.DefaultPrng.init(seed);
     var rand = prng.random();
 
-    const weight1 = rand.float(f32);
-    const weight2 = rand.float(f32);
-    const weight3 = rand.float(f32);
-
     const bias = 1;
-    var weights = [_]f32{ weight1, weight2, weight3 };
-
-    // std.debug.print("Weight 1 is : {d}\n", .{weight1});
-    // std.debug.print("Weight 2 is : {d}\n", .{weight2});
-    // std.debug.print("Weight 3 is : {d}\n", .{weight3});
+    var weights = [_]f32{ rand.float(f32), rand.float(f32), rand.float(f32) };
 
     std.debug.print("\nMy Weights are: {d}\n", .{weights});
 
@@ -33,9 +29,7 @@ pub fn main() !void {
         perceptron(&weights, bias, 0, 0, 0);
 
         std.debug.print("This is iteration : {d}\n", .{iterations});
-        std.debug.print("Weight 1 is : {d}|", .{weights[0]});
-        std.debug.print("Weight 2 is : {d}|", .{weights[1]});
-        std.debug.print("Weight 3 is : {d}\n", .{weights[2]});
+        std.debug.print("\nMy Weights are: {d}\n", .{weights});
 
         iterations += 1;
     }
@@ -44,14 +38,10 @@ pub fn main() !void {
 fn perceptron(weights: *[3]f32, bias: comptime_int, input1: f32, input2: f32, output: f32) void {
     var outputP: f32 = input1 * weights[0] + input2 * weights[1] + bias * weights[2];
 
-    // print(x, "or", y, "is : ", outputP)
-
     if (outputP > 0) {
         outputP = 1;
-        //std.debug.print("outputP is {}\t", .{outputP});
     } else {
         outputP = 0;
-        //std.debug.print("outputP is {}\n", .{outputP});
     }
 
     //std.debug.print("outputP is {}\t", .{outputP});
@@ -60,13 +50,10 @@ fn perceptron(weights: *[3]f32, bias: comptime_int, input1: f32, input2: f32, ou
     const outputError = output - outputP;
     //var outputerror: f32 = output - outputP;
 
-    std.debug.print("outputerror is : {d}\n", .{outputerror});
-    weights[0] += outputerror * input1 * learningRate;
-    weights[1] += outputerror * input2 * learningRate;
-    weights[2] += outputerror * bias * learningRate;
+    weights[0] += outputError * input1 * learningRate;
+    weights[1] += outputError * input2 * learningRate;
+    weights[2] += outputError * bias * learningRate;
     std.debug.print("outputP is {}\t", .{outputP});
     std.debug.print("output is {}\t", .{output});
-    // std.debug.print("Weight 1 is : {d}\n", .{weights[0]});
-    // std.debug.print("Weight 2 is : {d}\n", .{weights[1]});
-    // std.debug.print("Weight 3 is : {d}\n", .{weights[2]});
+    std.debug.print("outputerror is : {d}\n", .{outputError});
 }
