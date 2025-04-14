419678fe5a13d31e3fe5fa3833c432217df92c7b
William Rescigno <143554483+will2web@users.noreply.github.com>
2025-04-14 12:11:24 -0700

    Created "get_user_input" function
    Since I'm getting two inputs I have redundant code that I was able to put within its own function

--- Perceptron.zig
+++ Perceptron.zig
@@ -2,48 +2,37 @@ const std = @import("std");
 const filename = "C:/Users/wresc/ZigProjects/Neural Network/Neural-Network/weights.json";
 
 pub fn main() !void {
-    const stdin = std.io.getStdIn().reader();
-    const stdout = std.io.getStdOut().writer();
+    var inputs: [2]bool = undefined;
+    for (inputs, 0..) |_, i| {
+        inputs[i] = try get_user_input(i);
+    }
 
-    try stdout.print("Is input 1 true or false? ", .{});
+    const weights: [3]f32 = try get_weights_from_file();
+    try perceptron(&weights, inputs[0], inputs[1]);
+}
+
+fn get_user_input(input: usize) !bool {
+    const stdout = std.io.getStdOut().writer();
+    const stdin = std.io.getStdIn().reader();
+
     var decision1: [7]u8 = undefined;
+    try stdout.print("Is input {} true or false? ", .{input + 1});
     _ = try stdin.readUntilDelimiter(&decision1, '\n');
-    //COMMENT 1      use BELOW line along with Comment 2 to debug user input
-    //const result1: []const u8 = try stdin.readUntilDelimiter(&decision1, '\n');
-    //COMMENT 1      use ABOVE line along with Comment 2 to debug user input
     const decision1_slice = std.mem.sliceTo(&decision1, '\r');
+
     var decision1_value: bool = undefined;
     if (std.mem.eql(u8, decision1_slice, "true")) {
         decision1_value = true;
     } else {
         decision1_value = false;
     }
-    //COMMENT 2     use BELOW lines along with Comment 1 to debug user input
+
+    //              Use BELOW 3 lines to debug user input
+    //const result1: []const u8 = try stdin.readUntilDelimiter(&decision1, '\n');
     //const string1 = std.unicode.fmtUtf8(result1);
     //std.debug.print("string1 is {any}\n", .{string1});
-    //COMMENT 2     use ABOVE lines along with Comment 1 to debug user input
 
-    try stdout.print("Is input 2 true or false? ", .{});
-    var decision2: [7]u8 = undefined;
-    _ = try stdin.readUntilDelimiter(&decision2, '\n');
-    const decision2_slice = std.mem.sliceTo(&decision2, '\r');
-    var decision2_value: bool = undefined;
-    if (std.mem.eql(u8, decision2_slice, "true")) {
-        decision2_value = true;
-    } else {
-        decision2_value = false;
-    }
-
-    //COMMENT 3     BELOW debugging prints
-    //std.debug.print("File content: {s}\n\n", .{buffer});
-    //std.debug.print("Parsed: {any}\n\n", .{parsed.value});
-    //std.debug.print("Weight0: {any}\n\n", .{file_weights.Weight0});
-    //std.debug.print("trained_weights: {any}\n\n", .{trained_weights});
-    //COMMENT 3     ABOVE debugging prints
-
-    const weights: [3]f32 = try get_weights_from_file();
-    std.debug.print("weights: {any}\n", .{weights});
-    try perceptron(&weights, decision1_value, decision2_value, stdout);
+    return decision1_value;
 }
 
 fn get_weights_from_file() ![3]f32 {
@@ -68,10 +57,17 @@ fn get_weights_from_file() ![3]f32 {
 
     const file_weights = parsed.value;
 
+    //COMMENT 3     BELOW debugging prints
+    // std.debug.print("File content: {s}\n\n", .{buffer});
+    // std.debug.print("Parsed: {any}\n\n", .{parsed.value});
+    //std.debug.print("Weight0: {any}\n\n", .{file_weights.Weight0});
+    //COMMENT 3     ABOVE debugging prints
+
     return [3]f32{ file_weights.Weight0, file_weights.Weight1, file_weights.Weight2 };
 }
 
-fn perceptron(primed_weights: []const f32, input1: bool, input2: bool, stdout: anytype) !void {
+fn perceptron(primed_weights: []const f32, input1: bool, input2: bool) !void {
+    const stdout = std.io.getStdOut().writer();
     const bias = 1;
 
     const first_input: f32 = switch (input1) {
